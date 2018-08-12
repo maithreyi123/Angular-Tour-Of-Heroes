1419d859d45459722101974fe7376b4efc2e6efe
venkat-narahari <venkat.narahari96@gmail.com>
2018-08-12 18:20:07 +0530

    In memory database service created, mock heroes deleted Commit

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Goku' },
+      { id: 2, name: 'Vegeta' },
+      { id: 3, name: 'Naruto' },
+      { id: 4, name: 'Sasuke' },
+      { id: 5, name: 'Kakashi' },
+      { id: 6, name: 'Zoro' },
+      { id: 7, name: 'Luffy' },
+      { id: 8, name: 'Deku' },
+      { id: 9, name: 'AllMight' },
+      { id: 10, name: 'Edward Elric' }
+    ];
+    return {heroes};
+  }
+
+  constructor() { }
+}
